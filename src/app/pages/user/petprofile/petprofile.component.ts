77eaa770ef4fed195253ff04a2ccb7f0a17ce6c6
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-01-31 11:12:11 +0200

    Added one document page

--- src/app/pages/user/petprofile/petprofile.component.ts
+++ src/app/pages/user/petprofile/petprofile.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PetService } from 'src/app/modules/pet/services/pet.service';
 
 @Component({
 	templateUrl: './petprofile.component.html',
 	styleUrls: ['./petprofile.component.scss'],
 	standalone: false
 })
-export class PetprofileComponent {}
+export class PetprofileComponent {
+	petprofile = this._petService.doc(
+		this._router.url.replace('/petprofile/', '')
+	);
+
+	constructor(private _petService: PetService, private _router: Router) {}
+}
--- src/app/pages/user/petprofile/petprofile.module.ts
+++ src/app/pages/user/petprofile/petprofile.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
 	{
-		path: '',
+		path: ':pet_id',
 		component: PetprofileComponent
 	}
 ];
