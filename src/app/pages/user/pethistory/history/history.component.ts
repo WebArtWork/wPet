4cd043c7744b21ab98490224ff1acea455990a25
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-02-05 12:21:44 +0200

    code rework

--- src/app/pages/user/pethistory/history/history.component.ts
+++ src/app/pages/user/pethistory/history/history.component.ts
@@ -1,13 +1,65 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormService } from 'src/app/core/modules/form/form.service';
+import { FormInterface } from 'src/app/core/modules/form/interfaces/form.interface';
+import { TranslateService } from 'src/app/core/modules/translate/translate.service';
+import { petrecordFormComponents } from 'src/app/modules/petrecord/formcomponents/petrecord.formcomponents';
 import { Petrecord } from 'src/app/modules/petrecord/interfaces/petrecord.interface';
+import { PetrecordService } from 'src/app/modules/petrecord/services/petrecord.service';
+import { AlertService, CoreService } from 'wacom';
 
 @Component({
-  selector: 'app-history',
-  standalone: false,
-  
-  templateUrl: './history.component.html',
-  styleUrl: './history.component.scss'
+	selector: 'app-history',
+	standalone: false,
+
+	templateUrl: './history.component.html',
+	styleUrl: './history.component.scss'
 })
 export class HistoryComponent {
-    @Input() record: Petrecord;  
+	@Input() record: Petrecord;
+
+	@Output() load = new EventEmitter();
+
+	constructor(
+		private _translate: TranslateService,
+		private _petrecordService: PetrecordService,
+		private _alert: AlertService,
+		private _form: FormService,
+		private _core: CoreService
+	) {}
+
+	form: FormInterface = this._form.getForm(
+		'petrecord',
+		petrecordFormComponents
+	);
+
+	update(doc: Petrecord): void {
+		this._form
+			.modal<Petrecord>(this.form, [], doc)
+			.then((updated: Petrecord) => {
+				this._core.copy(updated, doc);
+
+				this._petrecordService.update(doc);
+			});
+	}
+
+	delete(doc: Petrecord): void {
+		this._alert.question({
+			text: this._translate.translate(
+				'Common.Are you sure you want to delete this petrecord?'
+			),
+			buttons: [
+				{
+					text: this._translate.translate('Common.No')
+				},
+				{
+					text: this._translate.translate('Common.Yes'),
+					callback: async (): Promise<void> => {
+						this._petrecordService.delete(doc).subscribe(() => {
+							this.load.emit();
+						});
+					}
+				}
+			]
+		});
+	}
 }
--- src/app/pages/user/pethistory/pethistory.component.ts
+++ src/app/pages/user/pethistory/pethistory.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormService } from 'src/app/core/modules/form/form.service';
+import { FormInterface } from 'src/app/core/modules/form/interfaces/form.interface';
+import { petrecordFormComponents } from 'src/app/modules/petrecord/formcomponents/petrecord.formcomponents';
 import { Petrecord } from 'src/app/modules/petrecord/interfaces/petrecord.interface';
 import { PetrecordService } from 'src/app/modules/petrecord/services/petrecord.service';
 
@@ -17,13 +20,36 @@ export class PethistoryComponent {
 
 	constructor(
 		private _petrecordService: PetrecordService,
-		private _route: ActivatedRoute
+		private _route: ActivatedRoute,
+		private _form: FormService
 	) {
 		this._route.paramMap.subscribe((params) => {
 			this.pet_id = params.get('pet_id') || '';
 		});
 
 		this.load();
+	}
+
+	form: FormInterface = this._form.getForm(
+		'petrecord',
+		petrecordFormComponents
+	);
+
+	create(): void {
+		this._form.modal<Petrecord>(this.form, {
+			label: 'Create',
+			click: async (created: unknown, close: () => void) => {
+				close();
+
+				this._preCreate(created as Petrecord);
+
+				this._petrecordService
+					.create(created as Petrecord)
+					.subscribe(() => {
+						this.load();
+					});
+			}
+		});
 	}
 
 	load(): void {
@@ -44,5 +70,13 @@ export class PethistoryComponent {
 		}
 
 		return query;
+	}
+
+	private _preCreate(petrecord: Petrecord): void {
+		delete petrecord.__created;
+
+		if (this.pet_id) {
+			petrecord.pet = this.pet_id;
+		}
 	}
 }
