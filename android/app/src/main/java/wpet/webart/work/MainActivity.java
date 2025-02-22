ec5b15b18d0da1d751dab61edaa35a399ae57258
MrachkovskyiOleksandr <knms1b22.mrachkovskyi@kpnu.edu.ua>
2025-02-22 12:02:33 +0200

    added capacitor and generated assets for android

---
+++ android/app/src/main/java/wpet/webart/work/MainActivity.java
@@ -0,0 +1,5 @@
+package wpet.webart.work;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'wpet.webart.work',
+	appName: 'wPet',
+	webDir: 'dist/app/browser'
+};
+
+export default config;
